2f2381f09afb640c807ade0a6c7b1194c0de6ff2
QuinioaAlejandro <alejandroquinioa@gmail.com>
2023-03-07 21:05:54 -0300

    correccion

--- 01_ejercicio.js
+++ 01_ejercicio.js
@@ -1,58 +1,160 @@
+const fs = require("fs");
+const files = "./files";
+const file = files + "/products.json";
+
+let products = [];
+
 class ProductManager {
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.nameFile = file;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+    this.id;
+  }
 
-    constructor(nombre,  precio, thumbnail, code, stock, descripcion,) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-        this.descripcion = descripcion;
-        this.id; 
+  async addProduct() {
+    if (!fs.existsSync(files)){await fs.promises.mkdir(files, { recursive: true });
+    await fs.promises.writeFile(file, JSON.stringify(products));}
+    let found = products.find((i) => i.code == this.code);
+    if (found) {
+      console.log("cannot push products with the same code ");
+    } else {
+      if (
+        this.title == undefined ||
+        this.description == undefined ||
+        this.price == undefined ||
+        this.stock == undefined ||
+        this.code == undefined ||
+        this.thumbnail == undefined
+      ) {
+        console.log("product is incomplete");
+      } else {
+      products.push(this);
+        this.id = products.length;
+        await fs.promises.writeFile(file, JSON.stringify(products));
+      }
     }
+      
+      
+  }
 
-  addProduct() {
-   
-        let find = array.find((el) => el.code == this.code)
+  static async getProducts() {
+    if (products.length == 0) {
+      console.log(products)
+    } else {
+      if (fs.existsSync(file)){
+        let string = await fs.promises.readFile(file, "utf-8");
+      let content = JSON.parse(string);
+      console.log(content)
+      }
+    }
+  }
 
-        if (find){
-            console.log("error")
-        }
-        else {
-            array.push(this);
-        }
-        this.id = array.length
+  static async getProductById(id) {
+    let string = await fs.promises.readFile(file, "utf-8");
+    let content = JSON.parse(string);
+    let found = content.find((el) => el.id == id);
+    if (found) {
+    console.log(found);
+    } else {
+      console.log("Item's not been found.");
     }
+  }
 
-   getProduct(A) {
-    let find = array.find((el) => el.id == A)
-       if (find){
-        return this
-       }
-    else{
-        console.log("ese producto no existe")
+  static async updateProduct(id, change, desc) {
+    let string = await fs.promises.readFile(file, "utf-8");
+    let content = JSON.parse(string);
+    let found = content.find((el) => el.id == id);
+    if (found) {
+      if (change == "id") {
+        console.log("ID's cannot be modified.");
+      } else {
+        found[change] = desc;
+        products = content.filter((el) => el.id != id);
+        products.push(found);
+        await fs.promises.writeFile(file, JSON.stringify(products));
+      }
+    } else {
+      console.log("Product cannot be updated, as it does not exist.");
     }
-    }
+  }
 
-    getProducts(){
-        return array;
+  static async deleteProduct(id) {
+    let string = await fs.promises.readFile(file, "utf-8");
+    let content = JSON.parse(string);
+    let found = content.find((el) => el.id == id);
+    if (found) {
+      let filter = content.filter((el) => el.id != id);
+      products = filter;
+      await fs.promises.writeFile(file, JSON.stringify(products));
+    } else {
+      console.log("Product cannot be deleted, as it does not exist.");
     }
+  }
+
 }
 
-const array = []
+ProductManager.getProducts();
 
+const i1 = new ProductManager(
 
-const ProductManager1 = new ProductManager("dynamo", 100, "imagen.jpg", "chd45wj9", 20, "vinilo")
-const ProductManager2 = new ProductManager("Kid A Mnesia", 400, "imagen2.jpg", "cjs89sk0", 5,  "cassette")
-const ProductManager3 = new ProductManager("garbage", 200, "imagen3.jpg", "cjs89sk0", 6,  "Cd")
+  "prueba 1",
+  "esta es la prueba 1",
+  200,
+  "sin imagen",
+  "abc444",
+  24
+);
+const i2 = new ProductManager(
 
+  "prueba 2",
+  "esta es la prueba 2",
+  600,
+  "sin imagen",
+  "abc594",
+  24
+);
+const i3 = new ProductManager(
 
-ProductManager1.addProduct();
-ProductManager2.addProduct();
-ProductManager3.addProduct();
+  "prueba 3",
+  "esta es la prueba 3",
+  800,
+  "sin imagen",
+  "abc444",
+  24
+);
 
+const i4 = new ProductManager("prueba 4", "esta es la prueba 4");
 
+const i5 = new ProductManager(
 
-console.log(ProductManager1.getProducts());
+  "prueba 5",
+  "esta es la prueba 5",
+  700,
+  "sin imagen",
+  "abc504",
+  455
+);
 
-console.log(ProductManager1.getProduct(1));
+i1.addProduct();
+i2.addProduct();
+i3.addProduct();
+i4.addProduct();
+i5.addProduct();
 
+ProductManager.getProducts();
+
+ProductManager.getProductById(1);
+ProductManager.getProductById(5);
+
+ProductManager.updateProduct(1, "code", "abx333");
+ProductManager.updateProduct(8, "code", "abx333");
+
+setTimeout(() => {
+  ProductManager.deleteProduct(2);
+  ProductManager.deleteProduct(7);
+}, 2000);
